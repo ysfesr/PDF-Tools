60c6884db15cdc6a4dc4aca437d9d0ce6b203270
youssef elasery <joseph.elasery@gmail.com>
2021-02-23 10:55:34 +0100

    add a landing page

--- core/urls.py
+++ core/urls.py
@@ -24,4 +24,4 @@ urlpatterns = [
     path('', include('pdf_tools.urls')),
 ] 
 
-#+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
